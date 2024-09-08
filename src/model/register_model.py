9f04f20be9bb8cbbd41b483b493bfdbd3b662d6b
sylashalder <sylashalderb@gmail.com>
2024-09-08 10:52:17 +0600

    added test folder and edit ci

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -68,7 +68,7 @@ def register_model(model_name: str, model_info: dict):
         client.transition_model_version_stage(
             name=model_name,
             version=model_version.version,
-            stage="Production"
+            stage="Staging"
         )
         
         logger.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
